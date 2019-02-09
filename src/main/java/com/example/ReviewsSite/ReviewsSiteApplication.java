332441a21aaf932888aae4e8d51e5b6c6ee9edf2
ryanandrewmcpherson <ryanmcphersondev@gmail.com>
2019-02-09 12:54:14 -0500

    first commit

---
+++ src/main/java/com/example/ReviewsSite/ReviewsSiteApplication.java
@@ -0,0 +1,14 @@
+package com.example.ReviewsSite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewsSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewsSiteApplication.class, args);
+	}
+
+}
+
